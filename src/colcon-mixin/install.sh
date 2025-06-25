43b027f1621ea2916c27c36dad1f1f0ab1f26eb2
David Oberacker <oberacker@fzi.de>
2025-06-25 14:31:19 +0200

    Fix colcon feature.

--- src/colcon-mixin/install.sh
+++ src/colcon-mixin/install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
-colcon mixin add default https://raw.githubusercontent.com/colcon/colcon-mixin-repository/master/index.yaml
+repo_line="default: https://raw.githubusercontent.com/colcon/colcon-mixin-repository/master/index.yaml"
+
+if ! colcon mixin list | grep -qF "$repo_line"; then
+  colcon mixin add default https://raw.githubusercontent.com/colcon/colcon-mixin-repository/master/index.yaml
+fi
+
 colcon mixin update default
 
